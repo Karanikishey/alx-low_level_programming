074592540e2cffa1d8255ad9aeca1b9fc52ce9d2
Karanikishey <melkarani@gmail.com>
2022-03-22 14:48:49 +0300

    string

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates integer value to 98
+ *@n: A pointer to an int that will be updated
+ *
+ [1;5x[3;1;1;120;120;1;0x*Return: void that means is correct
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}

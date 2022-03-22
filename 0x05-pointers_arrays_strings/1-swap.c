87cd406e326e0ce395bb6058cdaf8af9bbea4849
Karanikishey <melkarani@gmail.com>
2022-03-22 18:41:01 +0300

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of integers.
+ *
+ *@a: number 1.
+ *
+ *@b: number 2.
+ *
+ * Return: 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c =[A[B *a;
+*a = *b;
+*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s)
-void _puts(char *str);;[B[B[C
-: void reset_to_98(int *n);[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cvoid reset_to_98(int *n);
+void _puts(char *str);
+: void reset_to_98(int *n)
